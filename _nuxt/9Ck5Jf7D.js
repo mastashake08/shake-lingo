45a7cf0d4a53f4e3027e625f26fd74cdf14fb2ca
mastashake08 <mastashake08@users.noreply.github.com>
2024-12-03 17:03:09 +0000

    deploy: 3e19fa3f2f0c8c6433890d92afff04b3274c98e8

---
+++ _nuxt/9Ck5Jf7D.js
@@ -0,0 +1,1 @@
+import{r as t,s as o}from"./DemqZqsT.js";const c=()=>{const e=t([]),n=()=>{const a=speechSynthesis.getVoices();e.value=a.map(s=>({code:s.lang,name:`${s.name} (${s.lang})`}))};return o(()=>{n(),e.value.length||(speechSynthesis.onvoiceschanged=n)}),{languages:e}};export{c as u};
